61c20f6126613f04fdd271c20d9fcd54a6b6263c
clarson98 <55099597+clarson98@users.noreply.github.com>
2021-04-14 16:23:59 -0400

    Removed settings page

--- app/src/main/java/com/example/driver_1/data/loggedInDriver/MainActivity.java
+++ app/src/main/java/com/example/driver_1/data/loggedInDriver/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements CategoryFragment.
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_store, R.id.nav_settings, R.id.nav_logout)
+                R.id.nav_home, R.id.nav_store, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
